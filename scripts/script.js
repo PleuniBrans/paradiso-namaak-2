8638e093ac57bf69228cce55480ea600a6df5a31
PleuniBrans <152277954+PleuniBrans@users.noreply.github.com>
2024-12-06 09:44:27 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -17,36 +17,3 @@ menuButton.addEventListener("click", () => {
 
 
 
-const carousel = document.querySelector('section ul');
-let isMouseDown = false;
-let startX;
-let scrollLeft;
-
-// Mouse down event to start dragging
-carousel.addEventListener('mousedown', (e) => {
-  isMouseDown = true;
-  startX = e.pageX - carousel.offsetLeft;
-  scrollLeft = carousel.scrollLeft;
-  carousel.style.cursor = 'grabbing';  // Change cursor to grabbing
-});
-
-// Mouse leave event to stop dragging
-carousel.addEventListener('mouseleave', () => {
-  isMouseDown = false;
-  carousel.style.cursor = 'grab';  // Reset cursor to grab
-});
-
-// Mouse up event to stop dragging
-carousel.addEventListener('mouseup', () => {
-  isMouseDown = false;
-  carousel.style.cursor = 'grab';  // Reset cursor to grab
-});
-
-// Mouse move event for dragging
-carousel.addEventListener('mousemove', (e) => {
-  if (!isMouseDown) return;  // Only trigger when dragging
-  e.preventDefault();  // Prevent default behavior
-  const x = e.pageX - carousel.offsetLeft;
-  const walk = (x - startX) * 3;  // The multiplier controls the speed
-  carousel.scrollLeft = scrollLeft - walk;  // Adjust the scroll position
-});
